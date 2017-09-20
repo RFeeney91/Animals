911e437a33ddf383a7b42c86192190c98ed85618
RFeeney91 <ryansfeeney@gmail.com>
2017-09-20 13:13:21 -0700

    Update hw.js

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,5 +1,5 @@
  // Example queryURL for Giphy API
-var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=f28d3740e4d24ba284bd6da55330e1d8";
     $.ajax({
       url: queryURL,
       method: 'GET'
