ea9ef08fe13cba9e61f859ffbca902480e5ca077
RFeeney91 <ryansfeeney@gmail.com>
2017-09-20 13:04:34 -0700

    Update hw.js

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -7,3 +7,14 @@ var animal-input
 var animals
 
 var addAnimals
+
+
+
+    // Example queryURL for Giphy API
+    var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+    });
